04616a4a244d80c9c2603c674892a922d4aaa135
ankit <sindeadly25@gmail.com>
2021-11-15 09:25:42 +0530

    created git repo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const datafile = 0;
+  let datafile = [
+    { id: "1", home: "ghar", age: "69" },
+    { id: "2", home: "dusra ghar", age: "70" },
+    { id: "3", home: "tesra ghar", age: "71" },
+    { id: "4", home: "chautha ghar", age: "72" },
+    { id: "5", home: "pachwa ghar", age: "73" },
+    { id: "6", home: "chatwa ghar", age: "74" },
+  ];
 
   return (
     <div className="App">
       {datafile.map((datainfo) => {
-        return <h1>hii</h1>;
+        return <h1>{datainfo.id}</h1>;
       })}
     </div>
   );
